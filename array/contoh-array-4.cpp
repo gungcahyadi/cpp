d2878bf77fac8cce91384dead45475e57c35c831
gungcahyadi <gungcahyadipp@gmail.com>
2019-11-18 18:55:22 +0800

    simpan input dalam array
    menimpan inputan dari user dalam array

---
+++ array/contoh-array-4.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+	// input array dari user
+
+    int nilai[4];
+    for (int i = 0; i < 4; ++i)
+    {
+    	cin >> nilai[i];
+    }
+    for (int i = 0; i < 4; ++i)
+    {
+    	cout << nilai[i] << "\n";
+    }
+
+
+    return 0;
+}
