16f7891a25b27a206a01693a3e391959fd1ee839
gungcahyadi <gungcahyadipp@gmail.com>
2019-11-28 13:58:05 +0800

    Update contoh-array-7-ordo-dari-user.cpp

--- array/contoh-array-7-ordo-dari-user.cpp
+++ array/contoh-array-7-ordo-dari-user.cpp
@@ -1,44 +1,50 @@
-#include <iostream>
-using namespace std;
+/**
+ *  Buatlah sebuah program yang menerima jumlah ordo matriks dari user, 
+ *  kemudian berikan nilai pada matriks tersebut sesuai dengan jumlah ordonya, 
+ *  langkah terakhir tampilkan kembali matriks yang sudah berisi nilai tersebut..
+ */
 
-int main ()
+/**
+ * PENULISAN HEADER STANDAR
+ */
+#include <iostream> // memanggil standar library iostream
+using namespace std; // ini adalah sebuah penyingkat atau penyederhana dari notasi yang ada dalam Library iostream
+int main () // ini adalah fungsi utama program (body)
 {
     /**
-     *  Buatlah sebuah program yang menerima jumlah ordo matriks dari user, 
-     *  kemudian berikan nilai pada matriks tersebut sesuai dengan jumlah ordonya, 
-     *  langkah terakhir tampilkan kembali matriks yang sudah berisi nilai tersebut..
+     * DEKLARASIKAN VARIABLE
      */
-
-    int nilai[10][10];
-    int ordo,ordo2;
-    cout << "Silakan masukkan Ordo 1 (baris): ";
-    cin >>  ordo;
-    cout << "Silakan masukkan Ordo 2 (kolom): ";
-    cin >>  ordo2;
-
-    for (int i = 0; i < ordo; ++i)
-    {
-        for (int j = 0; j < ordo2; ++j)
-        {
-            cout << "Nilai Baris ke-"<< i+1 << " Kolom ke-"<< j+1<<" : ";
-            cin >> nilai[i][j];
+    int nilai[10][10]; // mendeklarasikan variable nilai dengan array 2 dimensi yang masing masing memiliki 10 index
+    int ordo,ordo2; // deklarasikan variable ordo 1 dan ordo 2
+    /**
+     * MEMINTA USER UNTUK MENGINPUTKAN JUMLAH ORDO
+     */
+    cout << "Silakan masukkan Ordo 1 (baris): "; // untuk menampilkan text (Silakan masukkan Ordo 1 (baris): )
+    cin >>  ordo; // untuk input ordo 1 dari user
+    cout << "Silakan masukkan Ordo 2 (kolom): "; // untuk menampilkan text (Silakan masukkan Ordo 2 (kolom): )
+    cin >>  ordo2; // untuk input ordo 2 dari user
+    /**
+     * PROSES INPUT NILAI DARI JUMLAH INPUT ORDO DARI USER
+     */
+    for (int i = 0; i < ordo; ++i) { // fungsi looping berdasarkan dari nilai ordo 1
+        for (int j = 0; j < ordo2; ++j) { // fungsi looping berdasarkan dari nilai ordo 2
+            cout << "Nilai Baris ke-"<< i+1 << " Kolom ke-"<< j+1<<" : "; // untuk menampilkan text ( baris ke berapa kolom ke berapa )
+            cin >> nilai[i][j]; // untuk input nilai
         }
     }
-
-    cout << "=======================\n";
-    cout << "Hasilnya adalah: \n";
-    cout << "=======================";
-    for (int i = 0; i < ordo; ++i)
-    {
-        for (int j = 0; j < ordo2; ++j)
-        {
-            if (j == 0)
-            {
-                cout << "\n";
+    /**
+     * MENAMPILKAN HASIL NILAI DARI INPUT USER
+     */
+    cout << "=======================\n"; // untuk menampilkan text (dekorasi)
+    cout << "Hasilnya adalah: \n"; // untuk menampilkan text (Hasilnya adalah :)
+    cout << "======================="; // untuk menampilkan text (dekorasi)
+    for (int i = 0; i < ordo; ++i) { // fungsi looping berdasarkan dari nilai ordo 1 
+        for (int j = 0; j < ordo2; ++j) { // fungsi looping berdasarkan dari nilai ordo 2
+            if (j == 0){ // cek kondisi, bila nilai j sama dengan 0 maka diberikan enter
+                cout << "\n"; // memberikan enter
             }
-            cout << nilai[i][j] << " ";
+            cout << nilai[i][j] << " "; // menampilkan nilai
         }
     }
-
-    return 0;
+    return 0; // mengembalikan nilai balik
 }
