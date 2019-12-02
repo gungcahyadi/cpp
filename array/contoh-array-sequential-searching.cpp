27ae4382a141ab79c79f70cb7730b895db436761
gungcahyadi <gungcahyadipp@gmail.com>
2019-12-02 18:39:35 +0800

    Create contoh-array-sequential-searching.cpp

---
+++ array/contoh-array-sequential-searching.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+	int n; // deklarasi variable n
+	cout << "[10,2,8,5,3]\n"; // menampilkan data array yang ada
+    int ar[5] = {10,2,8,5,3}; // data array
+    cin >>  n; // input angka yang dicari
+    for (int i = 0; i < 5; ++i) // lopping data array
+    {
+    	if (ar[i] == n) // cek anggka yang dicari
+    	{
+    		cout << "angka " << sc << " ditemukan di index " << i+1; // tampilkan text bila ditemukan 
+    		break; // stop program
+    	}
+    }
+
+    return 0;
+}
